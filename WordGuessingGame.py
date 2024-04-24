62e975d03de4a106dbedc15b21be3942c08b1d2c
Sridhar <kevinsridhar016@gmail.com>
2024-04-23 23:36:15 -0400

    final version

--- WordGuessingGame.py
+++ WordGuessingGame.py
@@ -9,44 +9,50 @@ import random
 
 
 
-numPlayers = args[0]
+numPlayers = int(args[0])
 if numPlayers<1: exit(0)
 
 playerStats = [[0, 0] for i in range(numPlayers)] # each player has a list of [num letter guesses, num word guesses]
 
-wL = open("WordleWords.txt", 'r').read() #list of words
+wL = open("words.txt", 'r').read().splitlines() #list of words
 
 rInt = random.randint(0, len(wL))
+print(rInt)
 word = wL[rInt]
+print(word)
 endedGames = set()
+flag = True
 
-while True:
+while True and flag:
+    if len(endedGames) == numPlayers: break
     for i in range(1, numPlayers+1):
         if i in endedGames: continue
         
-        inp = input("Player " + str(i) + "\Word Guess? (y/n)")
+        inp = input("Player " + str(i) + "\nWord Guess? (y/n) ")
         if inp.lower() not in {*"yn"}:
             print("Invalid input")
             exit(0)  
         
         
-        letterGuess = input("Give a letter guess")
-        print("Number of " + letterGuess + " in word is " + str(word.count(letterGuess.lower())))
-        playerStats[i][0]+=1
+        letterGuess = input("Give a letter guess: ")
+        print("Number of " + letterGuess + "'s in word is " + str(word.count(letterGuess.lower())))
+        playerStats[i-1][0]+=1
             
         if inp.lower() == "y":
-            wordGuess = input("Give a word guess")
+            wordGuess = input("Give a word guess: ")
             if wordGuess == word:
                 print("Congratulations you guessed the correct word: " + word)
-                print("Score for player " + str(i)+ ": " + str(playerStats[i][0]))
+                print("Score for player " + str(i)+ ": " + str(playerStats[i-1][0]))
+                flag = False
+                break
                 
             else:
                 print("Incorrect guess")
-                playerStats[i][1]+=1
-                if playerStats[i][1] >=3:
+                playerStats[i-1][1]+=1
+                if playerStats[i-1][1] >=3:
                     print("You have used all your word guesses and your game is over.")
                     endedGames.add(i)
             
             
-        
+print("Thank you for playing and have a great day")
 
